28a7331e869285fe14f5b49e87ff84c8f6fdbc07
Juan Andrade <49418565+juan-20@users.noreply.github.com>
2021-01-05 15:40:34 -0300

    Arrumando a Bagunça :package:

---
+++ src/app/photos/photo/photo.ts
@@ -0,0 +1,11 @@
+export interface Photo{
+    // tipagem do tipo foto
+        id:number;
+        postDate:Date;
+        url:string;
+        description:string;
+        allowComments:boolean;
+        likes:number;
+        comments:number;
+        userId:number;
+}
